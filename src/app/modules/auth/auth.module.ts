64cebc6e1c83414628e464f3c062e7bdc65468ed
Yanis Adekalom <yanis971.60@gmail.com>
2023-11-09 16:17:09 +0100

    Feat: Auth Module Register

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,15 +4,18 @@ import { LoginComponent } from './components/login/login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {RouterModule, Routes} from "@angular/router";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { RegisterComponent } from './components/register/register.component';
 
 const route : Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ]
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
